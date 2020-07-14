6c5ce61ff7eb9ad0028e8ab6e08fa4f6fa481aeb
irfan hadian <irfan.hadian10@gmail.com>
2020-07-14 12:58:54 +0700

    added compose for redux dev tool

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 // create store
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
